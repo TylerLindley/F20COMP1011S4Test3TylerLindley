6deb7187f33b1caf2811c52966903f7bfe666758
Tyler <200382154@student.georgianc.on.ca>
2020-12-18 11:27:02 -0500

    challenge 2-JsonResponse

--- src/Models/JsonFileResult.java
+++ src/Models/JsonFileResult.java
@@ -1,4 +1,30 @@
 package Models;
 
 public class JsonFileResult {
+    private String dateGenerated, examStatus;
+    private BloodDonor[] bloodDonors;
+
+    public String getDateGenerated() {
+        return dateGenerated;
+    }
+
+    public void setDateGenerated(String dateGenerated) {
+        this.dateGenerated = dateGenerated;
+    }
+
+    public String getExamStatus() {
+        return examStatus;
+    }
+
+    public void setExamStatus(String examStatus) {
+        this.examStatus = examStatus;
+    }
+
+    public BloodDonor[] getBloodDonors() {
+        return bloodDonors;
+    }
+
+    public void setBloodDonors(BloodDonor[] bloodDonors) {
+        this.bloodDonors = bloodDonors;
+    }
 }
